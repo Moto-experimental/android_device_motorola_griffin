08cf931f5b11d4d88aaff484f134935a50ae09e6
Ethan Chen <intervigil@gmail.com>
2020-03-01 00:34:32 -0800

    griffin: Remove unnecessary netutils packages
    * These are already packaged by AOSP.
    
    Change-Id: I77f91fbbb33a6d891d14fe852b094ab914ca3111

--- device.mk
+++ device.mk
@@ -277,8 +277,7 @@ PRODUCT_PACKAGES += \
 # Net
 PRODUCT_PACKAGES += \
     android.system.net.netd@1.0 \
-    libandroid_net \
-    netutils-wrapper-1.0
+    libandroid_net
 
 # NFC
 PRODUCT_PACKAGES += \
