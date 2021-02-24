a601502de6d6c3b1a177a8b958204e350ea98524
Quallenauge <Hamsi2k@freenet.de>
2021-02-24 19:40:28 +0100

    griffin: Exclude CarrierConfig overlays from RRO
    They doesn't work right, because xml attributes where
    stripped down, and leading zeros are removed.
    
    E.g. instead of
     <carrier_config mcc="262" mnc="06">
    the values are modified and get
     <carrier_config mcc="262" mnc="6">
    
    This prevents matching of defined carrier definitions.
    
    Change-Id: I11efe716f9d2ea44bef80c15004d7a8cb71b58b9

--- lineage_griffin.mk
+++ lineage_griffin.mk
@@ -34,6 +34,8 @@ DEVICE_PACKAGE_OVERLAYS += device/motorola/griffin/overlay
 endif
 
 PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    device/motorola/griffin/overlay/packages/apps/CarrierConfig
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2560
