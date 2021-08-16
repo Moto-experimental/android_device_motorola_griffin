f26152e715be1771205b80cf4c6addbdecee26fa
SirRGB <liam_wagner@gmx.de>
2021-08-16 12:28:49 +0200

    switch sources to Xtended

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_griffin.mk
+    $(LOCAL_DIR)/xtended_griffin.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_griffin-user \
-    lineage_griffin-userdebug \
-    lineage_griffin-eng
+    xtended_griffin-user \
+    xtended_griffin-userdebug \
+    xtended_griffin-eng
