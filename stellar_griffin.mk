ea2fde2306da0c32ddd34f931e6699623d62eb62
SirRGB <liam_wagner@gmx.de>
2021-08-17 15:58:11 +0200

    griffin: weebify

--- stellar_griffin.mk
+++ stellar_griffin.mk
@@ -23,12 +23,13 @@ $(call inherit-product, device/motorola/griffin/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/stellar/config/common_full_phone.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += device/motorola/griffin/overlay
+DEVICE_PACKAGE_OVERLAYS += device/motorola/griffin/overlay-weeb
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
@@ -42,7 +43,7 @@ TARGET_BOOTANIMATION_HALF_RES := true
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-PRODUCT_NAME := lineage_griffin
+PRODUCT_NAME := stellar_griffin
 PRODUCT_BRAND := motorola
 PRODUCT_DEVICE := griffin
 PRODUCT_MODEL := Moto Z
@@ -58,3 +59,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := motorola/griffin/griffin:8.0.0/OPL27.76-51/58:user/release-keys
 TARGET_VENDOR := motorola
+
+
+# additional Amy stuff
+TARGET_USES_BLUR := true
