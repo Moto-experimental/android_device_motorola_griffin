70bd652bbadced96f322af7d414f46d11e1a885f
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-10-12 16:27:19 +0100

    griffin: Use default keymaster@4.1 HIDL
    * We use software keymaster anyway, let's avoid letting keymaster HIDL down at every boot seeing that a secure keystore doesn't exists
    
    Change-Id: Ic597f98dfec91e476e08a0b9fc7d9a59da7084e1
    Signed-off-by: SirRGB <sirrgb@proton.me>

--- device.mk
+++ device.mk
@@ -242,8 +242,7 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.0-impl:64 \
-    android.hardware.keymaster@4.0-service
+    android.hardware.keymaster@4.1-service
 
 # Lights
 PRODUCT_PACKAGES += \
