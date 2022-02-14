000155a5a3cf4f4cdb4ca8d202c9a6e085154e72
Jeferson <jroliveira.oliveira301@gmail.com>
2022-02-14 12:38:23 +0100

    griffin: Build common MotoActions
    Change-Id: Ibef2d7e154da0f1581dfbc41bbfe11fb41938aae

--- device.mk
+++ device.mk
@@ -107,10 +107,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.btconfigstore@1.0.vendor
 
-# LineageActions
-PRODUCT_PACKAGES += \
-    LineageActions
-
 # Camera
 PRODUCT_PACKAGES += \
     libbson.vendor \
@@ -265,6 +261,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml
 
+# MotoActions
+PRODUCT_PACKAGES += \
+    MotoActions
+
 # ModService
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/whitelist_modservice.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/whitelist_modservice.xml
@@ -397,7 +397,8 @@ PRODUCT_PACKAGES += \
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
-    $(LOCAL_PATH)
+    $(LOCAL_PATH) \
+    hardware/motorola
 
 # Tethering
 PRODUCT_PACKAGES += \
