fc37e5fcf0c453fd28d455c3af6df30353c39f11
z3DD3r <z3dd3r@gmail.com>
2022-04-19 17:09:17 +0300

    griffin: hal: Uprev keymaster HIDL HAL to 4.1

--- device.mk
+++ device.mk
@@ -246,8 +246,7 @@ PRODUCT_COPY_FILES += \
 
 # Keymaster HAL
 PRODUCT_PACKAGES += \
-    android.hardware.keymaster@4.0-impl:64 \
-    android.hardware.keymaster@4.0-service
+    android.hardware.keymaster@4.1-service \
 
 # Lights
 PRODUCT_PACKAGES += \
