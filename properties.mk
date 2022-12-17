26da482a77d72777876fe15f8431b51544247726
Jun Wang <quic_junwan@quicinc.com>
2022-12-17 06:31:17 +0100

    griffin: Disable Skia tracing by default
    If the prop "debug.hwui.skia_atrace_enabled" is enabled, too many
    skia tracing calls will be printed in every frame. This has obvious
    overhead on performance/power, and can cause many janks. So disable
    it by default, and user can set it manually if necessary.

--- properties.mk
+++ properties.mk
@@ -128,6 +128,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.gralloc.enable_fb_ubwc=1 \
     debug.hwui.renderer=skiagl \
+    debug.hwui.skia_atrace_enabled=false \
     debug.sf.enable_transaction_tracing=false \
     debug.sf.predict_hwc_composition_strategy=0 \
     debug.sf.recomputecrop=0 \
