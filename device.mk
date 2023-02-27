c311cb8ba70d91ade924f3818b3c4181ea642a94
SirRGB <sirrgb@proton.me>
2023-02-27 22:17:59 +0100

    griffin: build libcutils_shim

--- device.mk
+++ device.mk
@@ -374,12 +374,12 @@ PRODUCT_COPY_FILES += \
 
 # Shims
 PRODUCT_PACKAGES += \
-    libcutils_shim \
     libjustshoot_shim \
     libjni_shim \
     libmemset_shim \
     libqsap_shim \
-    libgpu_mapper_shim
+    libgpu_mapper_shim \
+    libcutils_shim.vendor
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
