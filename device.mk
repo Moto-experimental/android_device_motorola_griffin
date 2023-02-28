fb58bb6f22e3a78d31a82305a325d8d5843a7bf4
SirRGB <sirrgb@proton.me>
2023-02-28 16:59:39 +0100

    Revert "griffin: Remove protobuf hax"
    This reverts commit 380909dda42cd4524ec3129c7c8cb7fddc5d2da2.

--- device.mk
+++ device.mk
@@ -308,6 +308,9 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-vendorcompat \
     libprotobuf-cpp-lite-vendorcompat
 
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v28/arm64/arch-arm-armv8-a/shared/vndk-core/libprotobuf-cpp-lite.so:$(TARGET_COPY_OUT_VENDOR)/lib/libprotobuf-cpp-lite-v28.so
+
 # QCOM
 PRODUCT_PACKAGES += \
     libqti_vndfwk_detect \
@@ -349,6 +352,8 @@ PRODUCT_PACKAGES += \
     android.hardware.radio@1.1 \
     android.hardware.radio.deprecated@1.0.vendor \
     librmnetctl \
+    libprotobuf-cpp-full \
+    libprotobuf-cpp-lite \
     libqsap_sdk
 
 # Seccomp policy
@@ -369,12 +374,12 @@ PRODUCT_COPY_FILES += \
 # Shims
 PRODUCT_PACKAGES += \
     libcutils_shim \
-    libcutils_shim.vendor \
     libjustshoot_shim \
     libjni_shim \
     libmemset_shim \
     libqsap_shim \
-    libgpu_mapper_shim
+    libgpu_mapper_shim \
+    libcutils_shim.vendor
 
 # Soong
 PRODUCT_SOONG_NAMESPACES += \
