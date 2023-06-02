6bf3e9a8c79283d6d507df9aea47e9260107a728
SirRGB <sirrgb@proton.me>
2023-06-02 16:42:33 +0200

    griffin: set TARGET_BOOT_ANIMATION_RES
    * [1mvendor/lineage/config/bootanimation.mk:23: [35mwarning: [0m[1mTARGET_BOOT_ANIMATION_RES is invalid or undefined, using generic bootanimation[0m

--- lineage_griffin.mk
+++ lineage_griffin.mk
@@ -35,9 +35,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
 
 # Boot animation
-TARGET_SCREEN_HEIGHT := 2560
-TARGET_SCREEN_WIDTH := 1440
-TARGET_BOOTANIMATION_HALF_RES := true
+TARGET_BOOT_ANIMATION_RES := 1440
 
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
